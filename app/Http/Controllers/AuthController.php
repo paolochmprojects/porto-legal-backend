b40397ec11f7d3b255e91f0266c0ee12cd51f741
paolochmprojects <paolo.dev.projects@gmail.com>
2024-07-11 15:55:38 -0500

    [ADD] swagger docs

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,9 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Info(
+ *      title="API Swagger",
+ *      version="1.0",
+ *      description="API CRUD Students"
+ * )
+ * 
+ * @OA\Server(
+ *      url="http://localhost:8000",
+ * )
+ */
 class AuthController extends Controller
 {
 
+    /**
+     * @OA\Post(
+     *      path="/api/login",
+     *      tags={"Auth"},
+     *      summary="Login",
+     *      description="Login",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *      )          
+     * )
+     */
     public function login(Request $request)
     {
         $validated = $request->validate([
@@ -24,7 +47,7 @@ class AuthController extends Controller
                 401
             );
         }
-        
+
         return response()->json(
             [
                 'token' => $token
@@ -33,19 +56,22 @@ class AuthController extends Controller
         );
     }
 
-    public function me (){
+    public function me()
+    {
         return response()->json([
             'user' => auth()->user()
         ]);
     }
 
-    public function refresh(){
+    public function refresh()
+    {
         return response()->json([
             'token' => auth()->refresh()
         ]);
     }
 
-    public function logout(){
+    public function logout()
+    {
         auth()->logout();
         return response()->json([
             'message' => 'User logged out successfully'
